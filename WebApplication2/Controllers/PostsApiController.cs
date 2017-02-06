bc67fa21d5d4bc044970403598f20e7c12757bc7
joetian <joetian@gmail.com>
2017-02-05 17:34:58 -0800

    initial test

---
+++ WebApplication2/Controllers/PostsApiController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PostsApiController : ApiController
+    {
+        private PostsContext db = new PostsContext();
+
+        // GET: api/PostsApi
+        public IQueryable<Post> GetPosts()
+        {
+            return db.Posts;
+        }
+
+        // GET: api/PostsApi/5
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> GetPost(int id)
+        {
+            Post post = await db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // PUT: api/PostsApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPost(int id, Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != post.PostId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PostsApi
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> PostPost(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Posts.Add(post);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = post.PostId }, post);
+        }
+
+        // DELETE: api/PostsApi/5
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> DeletePost(int id)
+        {
+            Post post = await db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            db.Posts.Remove(post);
+            await db.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PostExists(int id)
+        {
+            return db.Posts.Count(e => e.PostId == id) > 0;
+        }
+    }
+}
---
+++ WebApplication2/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
